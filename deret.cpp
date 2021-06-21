635627821fbc8201176b3eab5ed9d2a3b01c9d6d
IndarHr <indar3644@gmail.com>
2021-06-21 23:39:53 +0700

    KIT

---
+++ deret.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  int nilai[7];
+ 
+  nilai[1] = 2000000;
+  nilai[2] = 800000;
+  nilai[3] = 10000;
+  nilai[4] = 9000;
+  nilai[5] = 100;
+  nilai[6] = 20;
+  nilai[7] = 9;
+ 
+  cout << "" << nilai[1] <<endl;
+  cout << "" << nilai[2] <<endl;
+  cout << "" << nilai[3] <<endl;
+  cout << "" << nilai[4] <<endl;
+  cout << "" << nilai[5] <<endl;
+  cout << "" << nilai[6] <<endl;
+  cout << "" << nilai[7] <<endl;
+  
+  return 0;
+}
